2673b5d480fc02b6c0d87cbd4dd4e5056421ceed
Erfan Huq <erfanshazid@gmail.com>
2022-05-08 13:01:23 -0400

    Two Sum Solution

---
+++ twosum.py
@@ -0,0 +1,11 @@
+#TwoSum 
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        complements = dict()
+        for i in range(0, len(nums)):
+            if ( (target-nums[i]) in complements):
+                return [nums.index(target-nums[i]), i]
+            else:
+                complements[nums[i]] = 1
+    
