7c33eed54151fc70fa0a1cf2d29b521ea9453071
Erfan Huq <erfanshazid@gmail.com>
2022-05-08 13:51:47 -0400

    median of two sorted arrays (attempt 3, own median function) faster

--- medianOfTwoSortedArrays.py
+++ medianOfTwoSortedArrays.py
@@ -2,6 +2,11 @@
 class Solution:
     def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
         nums1.extend(nums2)
-        nums = sorted(nums1)
-        return median(nums)
-    
+        arr = sorted(nums1)
+        n=len(arr)
+        m=n//2
+        if(n%2==0):
+            s=(arr[m-1]+arr[m])/2
+        else:
+            s=arr[m]
+        return s
