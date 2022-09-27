1485677b72d93a81c2f62fa60d362aeff7a621ad
Erfan Huq <53830580+EHuq@users.noreply.github.com>
2022-09-27 09:12:56 -0400

    Time: 39 ms (6.59%), Space: 10.4 MB (74.40%) - LeetHub

--- 328-odd-even-linked-list/328-odd-even-linked-list.cpp
+++ 328-odd-even-linked-list/328-odd-even-linked-list.cpp
@@ -3,28 +3,23 @@
  * struct ListNode {
  *     int val;
  *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
 class Solution {
 public:
-    ListNode* oddEvenList(ListNode* head){
-        if(head==NULL || head->next==NULL || head->next->next==NULL){
-            return head;
-        }else{
-            ListNode *odd=head;
-            ListNode *even = head->next;
-            ListNode *evenHead=even;
-            while(even!=NULL && even->next!=NULL)
-            {
-                odd->next = even->next;
-                odd = even->next;
-
-                even->next=odd->next;
-                even=odd->next;
-            }
-            odd->next=evenHead;
-            return head;
+    ListNode* oddEvenList(ListNode* head) {
+         if (head == nullptr) return nullptr;
+        ListNode* odd = head,*even = head->next, *evenHead = even;
+        while (even != nullptr && even->next != nullptr) {
+            odd->next = even->next;
+            odd = odd->next;
+            even->next = odd->next;
+            even = even->next;
         }
+        odd->next = evenHead;
+        return head;
     }
 };
